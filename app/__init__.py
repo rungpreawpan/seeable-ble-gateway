8f1377689db83e763fcc2b5da952891198a83901
Preawpan Siriphalangkanont <87581600+rungpreawpan@users.noreply.github.com>
2026-02-07 23:43:35 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,13 @@ from flask import Flask
 from .object_detection import object_detection
 from .midas_with_object_detect import obstacle_detection
 from .ar_markers_navigation import ar_markers_navigation
-
 from .scan_text import scan_text
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(object_detection)
     app.register_blueprint(obstacle_detection)
-    app.register_blueprint(scan_text)
     app.register_blueprint(ar_markers_navigation)
+    app.register_blueprint(scan_text)
     
     return app
